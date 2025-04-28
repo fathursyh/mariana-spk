c3e45ad91b8ec55280d1772e65c3fde060041752
Fathursyah Awandanu <fathur.syah1@gmail.com>
2025-04-28 14:38:16 +0700

    update : add vue drag and drop

---
+++ src/components/vue/criteria/DragDrop.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="bg-green-300 h-full flex">
+        <Container @drop="onDrop($event)"
+            :orientation="'vertical'"
+            :group-name="'element'"
+            :tag="{value: 'div', props: {class: 'tempat'}}"
+            @drag-start="startDrag"
+        >
+        
+        <Draggable v-for="item in container" :key="item">
+            <div class="w-52 h-32 bg-purple-300">
+                <p>{{ item }}</p>
+            </div>
+        </Draggable>
+        </Container>
+    </div>
+</template>
+
+<script setup>
+    import { Container, Draggable } from "vue-dndrop";
+    import { applyDrag } from 'src/lib/utils/helpers';
+    import { ref } from 'vue';
+
+
+    const container = ref(['tes', 'keren']);
+    const container2 = ref(['a']);
+    const onDrop = (dropResult) => {
+        container.value = applyDrag(container.value, dropResult);
+    };
+    const startDrag = (dragResult) => {
+        console.log(dragResult);
+    }
+
+</script>
